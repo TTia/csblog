9f702fc89441bc0497102271417065dbfd9a465a
Mattia Barrasso <mattia.barrasso@gmail.com>
2014-11-04 19:39:42 +0100

    Added project

---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        private CSBlogEntities db = new CSBlogEntities();
+
+        // GET: /Post/
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Author);
+            return View(posts.ToList());
+        }
+
+        // GET: /Post/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // GET: /Post/Create
+        public ActionResult Create()
+        {
+            ViewBag.authorId = new SelectList(db.Authors, "id", "email");
+            return View();
+        }
+
+        // POST: /Post/Create
+        // Per proteggere da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per ulteriori dettagli, vedere http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="id,authorId,title,body,createdAt,updatedAt")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.id = Guid.NewGuid();
+                db.Posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.authorId = new SelectList(db.Authors, "id", "email", post.authorId);
+            return View(post);
+        }
+
+        // GET: /Post/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.authorId = new SelectList(db.Authors, "id", "email", post.authorId);
+            return View(post);
+        }
+
+        // POST: /Post/Edit/5
+        // Per proteggere da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per ulteriori dettagli, vedere http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="id,authorId,title,body,createdAt,updatedAt")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.authorId = new SelectList(db.Authors, "id", "email", post.authorId);
+            return View(post);
+        }
+
+        // GET: /Post/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: /Post/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Post post = db.Posts.Find(id);
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Blog/Helper/SessionHelper.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Helper
+{
+    public class SessionHelper
+    {
+        public bool GrantAccess(string Username, string Password)
+        {
+            CSBlogDBEntities oDb = new CSBlogDBEntities();
+            var user = (from authors 
+                            in oDb.Authors 
+                        where authors.Email == Username && authors.HPassword == Password 
+                        select authors).First<Author>();
+            return (user != null);
+        }
+    }
+}
